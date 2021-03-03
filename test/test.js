20d17262a08c33f5fba427815292c4e72e058849
Alejandro Milán <alex.13062001des@gmail.com>
2021-03-03 04:38:33 -0600

    Test configured

--- test/test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var the_highest_word_1 = require("../the-highest-word");
-console.log(the_highest_word_1.theHighestWord('Hola mundo'));
-console.log(the_highest_word_1.theHighestWord('A Z'));
-console.log(the_highest_word_1.theHighestWord('Esta es una prueba de funcionamiento'));
-console.log(the_highest_word_1.theHighestWord('AaaaAa ZZZZzzzz'));
--- test/test.ts
+++
@@ -1,6 +0,0 @@
-import { theHighestWord } from '../the-highest-word'
-
-console.log(theHighestWord('Hola mundo'))
-console.log(theHighestWord('A Z'))
-console.log(theHighestWord('Esta es una prueba de funcionamiento'))
-console.log(theHighestWord('AaaaAa ZZZZzzzz'))
