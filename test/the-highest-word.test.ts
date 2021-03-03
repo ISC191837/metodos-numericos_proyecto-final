9b96db9e558ad98eaf66f424d3a28b8a1f823ff9
Alejandro Milán <alex.13062001des@gmail.com>
2021-03-03 04:55:01 -0600

    More tests added

--- test/the-highest-word.test.ts
+++ test/the-highest-word.test.ts
@@ -3,3 +3,11 @@ import { theHighestWord } from "../src/the-highest-word"
 test("El valor retornado debe ser 'z' con score de 26", () => {
   expect(theHighestWord("a z")).toEqual({ content: 'z', score: 26 })
 })
+
+test("El valor retornado debe ser 'c' con score de 2", () => {
+  expect(theHighestWord("b b a a a b c")).toEqual({ content: 'c', score: 2 })
+})
+
+test("El valor retornado debe ser 'h' con score de 7", () => {
+  expect(theHighestWord("a a b c a d e a g a H A E B a")).toEqual({ content: 'H', score: 7 })
+})
